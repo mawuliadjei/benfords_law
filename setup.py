426cdde78fa307126bc9a1eae8390cf21428a438
Mawuli <mawuli.adjei@ashesi.edu.gh>
2021-01-10 21:22:08 +0800

    hotfix: Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="benfords_law",
-    version="1.0.0.dev2",
+    version="1.0.0a1",
     description="Apply and run tests of Newcomb-Benford's Law on provided data",
     long_description=README,
     long_description_content_type="text/markdown",
