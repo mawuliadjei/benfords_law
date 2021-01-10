1631bffd16c47bd88cabe26abddd7506ba4ae0d4
Mawuli <mawuli.adjei@ashesi.edu.gh>
2021-01-10 16:05:32 +0800

    add set up files

---
+++ benfords_law/__init__.py
@@ -0,0 +1,2 @@
+# version of benfords_law package
+__version__ = "1.0.0"
---
+++ setup.py
@@ -0,0 +1,51 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="benfords_law",
+    version="1.0.0.dev1",
+    description="Apply and run tests of Newcomb-Benford's Law on provided data",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mawuliadjei/benfords_law",
+    author="Mawuli Adjei",
+    author_email="mawuliadjei@gmail.com",
+    license="MIT",
+    keywords='benfords law law of analogous numbers statistical tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Data Scientists',
+        'Topic :: Software Development :: Build Tools',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    project_urls={
+        # 'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+        # 'Funding': 'https://donate.pypi.org',
+        # 'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/mawuliadjei/benfords_law/',
+        'Tracker': 'https://github.com/mawuliadjei/benfords_law/issues',
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy==1.17.1',
+        'pandas==0.25.1',
+        'scipy==1.6.0',
+        'matplotlib==3.3.3',
+    ],
+    # entry_points={
+    #     "console_scripts": [
+    #         "benfords_law=benfords_law:main",
+    #     ]
+    # },
+    python_requires='>=3.6',
+)
